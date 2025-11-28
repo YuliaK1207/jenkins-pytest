3f38cbe1022c2a27cf8bc53bbcee9872066f2124
Yulia Kiiashkina <kiyashkina1207@gmail.com>
2025-11-28 20:45:26 +0200

    Added some more tests

--- test_first_test.py
+++ test_first_test.py
@@ -1,8 +1,12 @@
 def test_math_add():
-    assert 1+1 == 2
+    assert 1 + 1 == 2
 
 
 def test_math_minus():
-    assert 1-1 == 0
+    assert 1 - 1 == 0
+
+
+def test_math_multiply():
+    assert 3 * 1 == 3
 
     
